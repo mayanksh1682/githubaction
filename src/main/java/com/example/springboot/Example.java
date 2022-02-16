645f3276a8f9c8d80f137469e6f98fa942f77c58
mayanksh <mayanksh@gmail.com>
2022-02-16 18:57:51 -0500

    jacoco support

---
+++ src/main/java/com/example/springboot/Example.java
@@ -0,0 +1,15 @@
+package com.example.springboot;
+
+public class Example {
+	
+	 public long someRandomMethod(int a, int b) {
+	    	
+	    	if (a > 10 && b > 10){
+	    		return a+b;
+	    	}
+	    	else{
+	    		return a-b;
+	    	}
+	       
+	    }
+}
---
+++ src/test/java/com/example/springboot/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.example.springboot;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+	public void verifySomeRandomMethod() {
+		Example junitTest = new Example();
+		junitTest.someRandomMethod(10, 9);
+	}
+}
